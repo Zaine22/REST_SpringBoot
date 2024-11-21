55e55daa5ae00d7f0a5556403eac558c40c83bf8
Zaine <kyawkokozin223@gmail.com>
2024-11-21 14:30:44 +0630

    setup

---
+++ src/main/java/com/zaine/books/domain/BookEntity.java
@@ -0,0 +1,15 @@
+package com.zaine.books.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class BookEntity {
+
+    private String author;
+}
